d664b0f57d6d7198adf95f8a862d8bb7289b054b
kavipatel55 <kavipatel55@gmail.com>
2024-07-26 16:02:52 -0400

    working on user model and controller

---
+++ Data/Assignment3Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Assignment3.model;
+
+namespace Assignment3.Data
+{
+    public class Assignment3Context : DbContext
+    {
+        public Assignment3Context (DbContextOptions<Assignment3Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Assignment3.model.User> User { get; set; } = default!;
+    }
+}
